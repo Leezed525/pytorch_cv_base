8b445946e1bd408b41171cc51b9497a225e95abe
Leezed525 <leezed525@qq.com>
2024-04-04 20:12:39 +0800

    完成部分viptProcessing

--- lib/data/processing.py
+++ lib/data/processing.py
@@ -20,8 +20,40 @@ class BaseProcessing:
         self.transform = {
             'template': transform if template_transformer is None else template_transformer,
             'search': transform if search_transformer is None else search_transformer,
-            'joint': transform
+            'joint': joint_transformer
         }
 
     def __call__(self, data: TensorDict):
+        raise NotImplementedError
+
+
+class ViPTProcessing(BaseProcessing):
+    def __init__(self, search_area_factor, output_sz, center_jitter_factor, scale_jitter_factor, transform=transforms.ToTensor(),
+                 template_transformer=None, search_transformer=None, joint_transformer=None, mode='pair'):
+        """
+
+        :param search_area_factor:  The size of the search region  relative to the target size.
+        :param output_sz: An integer, denoting the size to which the search region is resized. The search region is always square.
+        :param center_jitter_factor: A dict containing the amount of jittering to be applied to the target center before extracting the search region. See _get_jittered_box for how the jittering is done.
+        :param scale_jitter_factor: A dict containing the amount of jittering to be applied to the target size before extracting the search region. See _get_jittered_box for how the jittering is done.
+        :param transform: see BaseProcessing
+        :param template_transformer: see BaseProcessing
+        :param search_transformer: see BaseProcessing
+        :param joint_transformer: see BaseProcessing
+        :param mode: Either 'pair' or 'sequence'. If mode='sequence', then output has an extra dimension for frames
+        """
+        super().__init__(transform, template_transformer, search_transformer, joint_transformer)
+        self.search_area_factor = search_area_factor
+        self.output_sz = output_sz
+        self.center_jitter_factor = center_jitter_factor
+        self.scale_jitter_factor = scale_jitter_factor
+        self.mode = mode
+
+    def _get_jittered_box(self, box, mode):
+        """
+        Jitter the input box
+        :param box: input bounding box
+        :param mode: string 'template' or 'search' indicating template or search data
+        :return: torch.Tensor - jittered box
+        """
         raise NotImplementedError
