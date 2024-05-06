dc8d828ac4b5344abb95947c782958d955c65ee9
Leezed525 <leezed525@qq.com>
2024-05-06 19:08:49 +0800

    一些处理

--- scripts/score_pureRMT_mlp_00005/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/lzd/workspace/LeeNet
-nohup python -u /home/lzd/workspace/LeeNet/train/score_pureRMT_mlp00005.py  >/home/lzd/workspace/LeeNet/logs/LeeNet_score_RMT_MLP00005/run.log 2>&1 &
-echo $! > ./scripts/score_pureRMT_mlp_00005/pid.txt
