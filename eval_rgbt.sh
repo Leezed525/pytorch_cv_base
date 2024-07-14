0f164cc4646ed24397bf7bb91068828578c30cd0
Leezed525 <leezed525@qq.com>
2024-07-14 22:56:05 +0800

    调整

--- eval_rgbt.sh
+++ eval_rgbt.sh
@@ -1,5 +1,5 @@
 # test lasher
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./test/test_rgbt_mgpus.py --script_name LeeNet --dataset_name LasHeR --yaml_name plscore_os_sigma_RGBT
+CUDA_VISIBLE_DEVICES=0,1,2,3 python ./RGBT_workspace/test_rgbt_mgpus.py --script_name LeeNet --dataset_name LasHeR --yaml_name plscore_os_sigma_RGBT
 
 # test rgbt234
 #CUDA_VISIBLE_DEVICES=0,1 python ./RGBT_workspace/test_rgbt_mgpus.py --script_name LeeNet --dataset_name RGBT234 --yaml_name plscore_os_sigma_RGBT
